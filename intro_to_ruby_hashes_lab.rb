15a02ba4639fb9f9ddb3d12030494beaf2a76326
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-20 20:07:34 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def new_hash
 end
 
 def actor
-	actor = {name: "Dwayne the Rock Johnson"}
+  actor = {name: "Dwayne the Rock Johnson"}
 
 end
 
@@ -24,7 +24,7 @@ def monopoly_with_second_tier
 	}   
 
 def monopoly_with_third_tier
-		monopoly = {
+	monopoly = {
 	   railroads: {
 		pieces: 4,
 		names: {
@@ -39,7 +39,7 @@ def monopoly_with_third_tier
 			:three_pieces_owned => 100,
 			:four_pieces_owned => 200,
 		}
-	   }
+	  }
 	} 
 end
 
@@ -61,7 +61,7 @@ def monopoly_with_fourth_tier
 				"mortgage_value" => "$800"
 			}
 		},
-		rent_in_dollars {
+		rent_in_dollars: {
 			:one_piece_owned => 25,
 			:two_pieces_owned => 50,
 			:three_pieces_owned => 100,
